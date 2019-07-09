9774b69dea793fda2a4d56aa9936955ef915c65d
PedroSchwarz <pedroschawarz97@gmail.com>
2019-07-09 20:19:20 -0300

    Landing page sign in form in progress

--- Código/assets/js/landing.js
+++ Código/assets/js/landing.js
@@ -1,5 +1,7 @@
-const slideBtn = document.querySelector('#slide-btn');
+const slideBtn = document.querySelector('#slide-btn')
 const slideshowSection = document.querySelector('#slideshow-section');
+const forms = document.querySelectorAll('form');
 slideBtn.addEventListener('click', (event) => {
     slideshowSection.classList.toggle('slide-up');
+    forms.forEach(el => el.classList.toggle('show-up'));
 });
