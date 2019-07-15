6669a059a1bf5fc8528a8d2bf751ee3fb8d05ec0
PedroSchwarz <pedroschawarz97@gmail.com>
2019-07-14 22:39:05 -0300

    Updates on Author Page e Landing Page

--- Código/assets/js/landing.js
+++ Código/assets/js/landing.js
@@ -1,7 +1,9 @@
-const slideBtn = document.querySelector('#slide-btn')
-const slideshowSection = document.querySelector('#slideshow-section');
-const forms = document.querySelectorAll('form');
+const slideBtn = document.querySelector('#slide-btn') // Selecionar Botão de Slide
+const slideshowSection = document.querySelector('#slideshow-section'); // Selecionar a Seção contendo o Slideshow
+const forms = document.querySelectorAll('form'); // Selecionar Todos os Formulários
 slideBtn.addEventListener('click', (event) => {
+    // Atribuir Classe para Animação de Rolagem Vertical Negativa
     slideshowSection.classList.toggle('slide-up');
-    forms.forEach(el => el.classList.toggle('show-up'));
+    // Para Cada Elemento de Formulário Atribuir Classe para Animação de Rolagem Vertical Negativa
+    forms.forEach(el => el.classList.toggle('slide-down'));
 });
